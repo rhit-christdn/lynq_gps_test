a170b27e34aff799796584fadcbdf800ea0b48f7
Christopherson <Dylan.Christopherson@ottoexcellence.com>
2025-06-26 13:44:28 -0500

    fixed loss of digits

--- main.cpp
+++ main.cpp
@@ -8,24 +8,28 @@
 
 using json = nlohmann::json;
 
-int main() {
-    const char* portName1 = "/dev/ttyACM0";
-    const char* portName2 = "/dev/ttyACM1";
+int main()
+{
+    const char *portName1 = "/dev/ttyACM0";
+    const char *portName2 = "/dev/ttyACM1";
 
     int serialPort1 = open(portName1, O_RDONLY | O_NOCTTY);
     int serialPort2 = open(portName2, O_RDONLY | O_NOCTTY);
 
-    if (serialPort1 < 0) {
+    if (serialPort1 < 0)
+    {
         std::cerr << "Error opening serial port 1\n";
         return 1;
     }
-    if (serialPort2 < 0) {
+    if (serialPort2 < 0)
+    {
         std::cerr << "Error opening serial port 2\n";
         return 1;
     }
 
     struct termios tty{};
-    if (tcgetattr(serialPort1, &tty) != 0) {
+    if (tcgetattr(serialPort1, &tty) != 0)
+    {
         std::cerr << "Error getting serial attributes for port 1\n";
         close(serialPort1);
         return 1;
@@ -44,12 +48,14 @@ int main() {
     tty.c_cc[VMIN] = 1;
     tty.c_cc[VTIME] = 0;
 
-    if (tcsetattr(serialPort1, TCSANOW, &tty) != 0) {
+    if (tcsetattr(serialPort1, TCSANOW, &tty) != 0)
+    {
         std::cerr << "Error setting serial attributes for port 1\n";
         close(serialPort1);
         return 1;
     }
-    if (tcsetattr(serialPort2, TCSANOW, &tty) != 0) {
+    if (tcsetattr(serialPort2, TCSANOW, &tty) != 0)
+    {
         std::cerr << "Error setting serial attributes for port 2\n";
         close(serialPort2);
         return 1;
@@ -58,54 +64,67 @@ int main() {
     std::string buffer1, buffer2;
     char chunk1[256], chunk2[256];
 
-    while (true) {
+    while (true)
+    {
         int n = read(serialPort1, chunk1, sizeof(chunk1) - 1);
         int m = read(serialPort2, chunk2, sizeof(chunk2) - 1);
 
-        if (n > 0) {
+        if (n > 0)
+        {
             chunk1[n] = '\0';
             buffer1 += chunk1;
 
-            size_t end = buffer1.find('}');
-            if (end != std::string::npos) {
+            // Parse all complete JSON messages
+            size_t end;
+            while ((end = buffer1.find('}')) != std::string::npos)
+            {
                 std::string message = buffer1.substr(0, end + 1);
                 buffer1.erase(0, end + 1);
 
-                try {
+                try
+                {
                     json j = json::parse(message);
-                    if (j.contains("lat") && j.contains("long")) {
+                    if (j.contains("lat") && j.contains("long"))
+                    {
                         double lat = j["lat"];
                         double lon = j["long"];
-                        std::cout << "Latitude 1: " << lat << ", Longitude 1: " << lon << "\n";
-                    } else {
-                        std::cerr << "Missing lat/long in JSON\n";
+                        std::cout << "Latitude 1: " << std::setprecision(10) << lat
+                                  << ", Longitude 1: " << std::setprecision(10) << lon << "\n";
                     }
-                } catch (json::parse_error& e) {
-                    std::cerr << "JSON parse error: " << e.what() << "\n";
+                }
+                catch (json::parse_error &e)
+                {
+                    std::cerr << "JSON parse error (1): " << e.what() << "\n";
                 }
             }
         }
 
-        if (m > 0) {
+        if (m > 0)
+        {
             chunk2[m] = '\0';
             buffer2 += chunk2;
 
-            size_t end = buffer2.find('}');
-            if (end != std::string::npos) {
+            // Parse all complete JSON messages
+            size_t end;
+            while ((end = buffer2.find('}')) != std::string::npos)
+            {
                 std::string message = buffer2.substr(0, end + 1);
                 buffer2.erase(0, end + 1);
 
-                try {
+                try
+                {
                     json j = json::parse(message);
-                    if (j.contains("lat") && j.contains("long")) {
+                    if (j.contains("lat") && j.contains("long"))
+                    {
                         double lat = j["lat"];
                         double lon = j["long"];
-                        std::cout << "Latitude 2: " << lat << ", Longitude 2: " << lon << "\n";
-                    } else {
-                        std::cerr << "Missing lat/long in JSON\n";
+                        std::cout << "Latitude 2: " << std::setprecision(10) << lat
+                                  << ", Longitude 2: " << std::setprecision(10) << lon << "\n";
                     }
-                } catch (json::parse_error& e) {
-                    std::cerr << "JSON parse error: " << e.what() << "\n";
+                }
+                catch (json::parse_error &e)
+                {
+                    std::cerr << "JSON parse error (2): " << e.what() << "\n";
                 }
             }
         }
